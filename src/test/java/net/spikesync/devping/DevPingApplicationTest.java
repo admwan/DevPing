9a708b9f3d8449e8cc2492f944396145ee02021b
Fred Wan <mail@fredwan.net>
2023-08-29 17:31:28 +0200

    Added tests for the SilverCloud scNode map (Nodes, IP addresses) with assertThat statements

--- src/test/java/net/spikesync/devping/DevPingApplicationTest.java
+++ src/test/java/net/spikesync/devping/DevPingApplicationTest.java
@@ -4,20 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import net.spikesync.devping.Pojo;
-import net.spikesync.devping.DevPingApplication;
-
 import org.springframework.context.ApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@EnableAutoConfiguration
-//@ImportResource({"classpath*:beans.xml"}) //The file isn't read
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DevPingApplication.class)
 public class DevPingApplicationTest {
@@ -26,15 +18,27 @@ public class DevPingApplicationTest {
 	private Pojo pojo;
 	@Value("${sample-value}")
 	private String sample;
+	@Autowired
+	private SilverCloud sc;
 
 	@Test
 	public void whenCallingGetter_thenPrintingProperty() {
 		assertThat(pojo.getField()).isNotBlank().isEqualTo(sample);
 	}
 
-//    @Test
-//    public void contextLoads(ApplicationContext context) {
-//    	assertThat(context).isNotNull();
-//    }
-
+    @Test
+    public void silverCloudTest(ApplicationContext context) {
+ 
+    	// this.sc is Autowired, so it is ready for testing!
+    	assertThat(sc.getScNodes())
+    			.isNotEmpty()
+    			.containsEntry("SURFIE","192.168.50.100")
+    			.containsEntry("CAPTUW","192.168.50.104")
+    			.containsEntry("THORFW","192.168.50.107")
+    			.containsEntry("LOKIFS","192.168.50.110")
+    			.containsEntry("SCARFS","192.168.50.113")
+    			.containsEntry("ASGAFS","192.168.50.119")
+    			.containsEntry("HYDRFS","192.168.50.116");
+    }
+    
 }
